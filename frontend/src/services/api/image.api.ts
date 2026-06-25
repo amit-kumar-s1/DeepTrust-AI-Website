db3261025841e824ac6be40400e31b763d287463
amit-kumar-s1 <themagnetoyt@gmail.com>
2026-06-25 06:04:59 +0530

    Added API

--- frontend/src/services/api/image.api.ts
+++ frontend/src/services/api/image.api.ts
@@ -1,10 +1,9 @@
 export async function detectImage(file: File) {
   const formData = new FormData();
-
   formData.append("file", file);
 
   const response = await fetch(
-    "http://localhost:8000/api/v1/detect/image",
+    "https://not-human-s1-deeptrust-ai.hf.space/api/v1/detect/image",
     {
       method: "POST",
       body: formData,
